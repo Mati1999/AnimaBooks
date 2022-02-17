9ed175763bd3d9bbc8cf2543b55cddb53b275719
Matias <aguileramati50@gmail.com>
2022-02-16 21:33:29 -0300

    Desafio del menu

---
+++ src/components/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../Styles/Menu.scss'
+import { FaBook } from 'react-icons/fa';
+
+const Menu = () => {
+    return (
+        <div className='menu'>
+            {/* <img className='menu__logo' src='' alt='logo' /> */}
+            <FaBook className='menu__logo' />
+            <ul className='menu__lista'>
+                <li><a href=''>Inicio</a></li>
+                <li><a href=''>Categorias</a></li>
+                <li><a href=''>Sobre nosotros</a></li>
+                <li><a href=''>Contacto</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu
